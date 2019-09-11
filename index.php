e9250f0a53b82c64e222b2ce0a55b2475fc39fd8
Pluginn <pramote.p.on@gmail.com>
2019-09-11 09:31:43 +0700

    Section2 Contact & Design

--- index.php
+++ index.php
@@ -60,12 +60,57 @@
             </div>
       </div>
         <h1>Pramote Phan-on (Plug)</h1>
-        <h3>23 November 1994</h3>
+        <h3>23 November 1994
+        <?php
+  //date in mm/dd/yyyy format; or it can be in other formats as well
+  $birthDate = "11/23/1994";
+  //explode the date to get month, day and year
+  $birthDate = explode("/", $birthDate);
+  //get age from date or birthdate
+  $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+    ? ((date("Y") - $birthDate[2]) - 1)
+    : (date("Y") - $birthDate[2]));
+  echo "(".$age.")";
+?>
+        </h3>
         <h3>FULL STACK WEB DEVELOPER</h3>
             <!-- <h3>23 November 1994</h3>
             <h3>Sex : Male</h3>
             <h3>Religion : Buddhism</h3>
             <h3>Nationality : Thai</h3> -->
+    </div>
+
+    <div class="row text-center pt-5">
+      <div class="col-md-2 movein2">
+        <a href="">
+          <i class="fab fa-facebook-square fa-3x"></i>
+        </a>
+      </div>
+      <div class="col-md-2 movein3">
+        <a href="">
+          <i class="fab fa-line fa-3x"></i>
+        </a>
+      </div>
+      <div class="col-md-2 movein4">
+        <a href="">
+          <i class="fas fa-phone-square fa-3x"></i>
+        </a>
+      </div>
+      <div class="col-md-2 movein5">
+        <a href="">
+          <i class="fab fa-instagram fa-3x"></i>
+        </a>
+      </div>
+      <div class="col-md-2 movein6">
+        <a href="">
+          <i class="fas fa-envelope fa-3x"></i>
+        </a>
+      </div>
+      <div class="col-md-2 movein7">
+        <a href="">
+          <i class="fab fa-github-square fa-3x"></i>
+        </a>
+      </div>
     </div>
   </section>
 
@@ -115,8 +160,15 @@
     ScrollReveal().reveal('.headline');
     ScrollReveal().reveal('.move-in', { duration: 1500,distance: '50px' });
     // ScrollReveal().reveal('.section1', { duration: 2000,distance: '50px' });
+
     ScrollReveal().reveal('.movein1', { duration: 1500,distance: '-100px' });
-    ScrollReveal().reveal('.movein2', { duration: 1500,delay: 500,distance: '150px' });
+    ScrollReveal().reveal('.movein2', { duration: 1000,delay: 500,distance: '150px' });
+    ScrollReveal().reveal('.movein3', { duration: 1500,delay: 500,distance: '150px' });
+    ScrollReveal().reveal('.movein4', { duration: 2000,delay: 500,distance: '150px' });
+    ScrollReveal().reveal('.movein5', { duration: 2500,delay: 500,distance: '150px' });
+    ScrollReveal().reveal('.movein6', { duration: 3000,delay: 500,distance: '150px' });
+    ScrollReveal().reveal('.movein7', { duration: 3500,delay: 500,distance: '150px' });
+
     ScrollReveal().reveal('.movein-left', { duration: 1500,distance: '100px',origin: 'left' });
     ScrollReveal().reveal('.movein-right', { duration: 1500,distance: '100px',origin: 'right' });
   </script>
